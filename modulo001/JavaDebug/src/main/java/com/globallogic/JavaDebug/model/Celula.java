3ded773e79698738f41d4cecd1340b02c8f97ef7
Juan Manuel Paniego <juanma.paniego@gmail.com>
2020-07-22 16:00:47 -0300

    Added JavaDebug test

---
+++ modulo001/JavaDebug/src/main/java/com/globallogic/JavaDebug/model/Celula.java
@@ -0,0 +1,30 @@
+package com.globallogic.JavaDebug.model;
+
+import java.util.ArrayList;
+
+public class Celula {
+	private int numero;
+    private Referente referente;
+    private ArrayList<Alumno> alumnos;      
+    
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public Referente getReferente() {
+        return referente;
+    }
+    public void setReferente(Referente referente) {
+        this.referente = referente;
+    }
+    public ArrayList<Alumno> getAlumnos() {
+        return alumnos;
+    }
+    public void setAlumnos(ArrayList<Alumno> alumnos) {
+        this.alumnos = alumnos;
+    }
+
+
+}
