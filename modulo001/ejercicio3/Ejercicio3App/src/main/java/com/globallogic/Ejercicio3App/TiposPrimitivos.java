17c1b8976288132a69065d366a2c74ea71f1f602
Juan Manuel Paniego <juanma.paniego@gmail.com>
2020-07-22 17:05:16 -0300

    Resolved first excersice

---
+++ modulo001/ejercicio3/Ejercicio3App/src/main/java/com/globallogic/Ejercicio3App/TiposPrimitivos.java
@@ -0,0 +1,23 @@
+package com.globallogic.Ejercicio3App;
+
+/**
+ * 
+ * @author jmpaniego
+ *
+ */
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+		int myNum = 15;
+		System.out.println("El valor de mi numero es " + myNum);
+		char myChar = 'a';
+		System.out.println("El valor de mi caracter es " + myChar);
+		boolean myBoolean = true;
+		System.out.println("El valor de mi boolean es " + myBoolean);
+		float myFloat = 2436.2456f;
+		System.out.println("El valor de mifloat es " + myFloat);
+		short myShort = 500;
+		System.out.println("El valor de mishort es " + myShort);
+	}
+
+}
