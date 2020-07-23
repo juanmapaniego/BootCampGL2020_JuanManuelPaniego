bf10653d02134061479b8adede1fa39ba2f292d9
Juan Manuel Paniego <juanma.paniego@gmail.com>
2020-07-23 12:52:31 -0300

    Resolved exercise 9

---
+++ modulo001/ejercicio4/Ejercicio4App/src/main/java/com/globallogic/Ejercicio9.java
@@ -0,0 +1,19 @@
+package com.globallogic;
+
+import java.util.stream.IntStream;
+
+/**
+ * 
+ * @author jmpaniego
+ *
+ */
+public class Ejercicio9 {
+
+	public static void main(String[] args) {
+		int[] numeros = {1,2,3,4,5,6,7,8,9,10};
+		for (int i : numeros) {
+			System.out.println("Valor iterado: " + i);
+		}
+	}	
+
+}
