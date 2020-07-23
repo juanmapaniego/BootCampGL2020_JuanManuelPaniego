96da43d571d076e54f6108d7161a9482b9bcd70c
Juan Manuel Paniego <juanma.paniego@gmail.com>
2020-07-23 12:40:56 -0300

    Resolved exercise 6

---
+++ modulo001/ejercicio4/Ejercicio4App/src/main/java/com/globallogic/Ejercicio6.java
@@ -0,0 +1,25 @@
+package com.globallogic;
+
+/**
+ * 
+ * @author jmpaniego
+ *
+ */
+public class Ejercicio6 {
+
+	public static void main(String[] args) {
+		String[] dias = {
+				"Lunes",
+				"Martes",
+				"Miercoles",
+				"Jueves",
+				"Viernes",
+				"Sabado",
+				"Domingo"
+						};
+		for (int i = 0; i < dias.length; i++) {
+			System.out.println("Dia de la semana: " + dias[i]);
+		}
+	}
+
+}
