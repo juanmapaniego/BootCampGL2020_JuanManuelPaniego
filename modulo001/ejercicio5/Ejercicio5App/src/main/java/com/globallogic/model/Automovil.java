a3ceee9181f8cabd09616e06925110ba51a72485
Juan Manuel Paniego <juanma.paniego@gmail.com>
2020-07-23 14:15:43 -0300

    Resolved exercise 4

---
+++ modulo001/ejercicio5/Ejercicio5App/src/main/java/com/globallogic/model/Automovil.java
@@ -0,0 +1,15 @@
+package com.globallogic.model;
+
+/**
+ * 
+ * @author jmpaniego
+ *
+ */
+public abstract class Automovil {
+	public abstract void retroceder();
+	
+	
+	public void avanzar() {
+		System.out.println("Soy el metodo avanzar de Automovil");
+	}
+}
