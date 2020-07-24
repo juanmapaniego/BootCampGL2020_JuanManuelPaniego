bfc2666d30bc80f162224461049a02b7e8d6cc15
Juan Manuel Paniego <juanma.paniego@gmail.com>
2020-07-24 12:21:22 -0300

    Resolved exercise 2

--- modulo001/ejercicio8/Ejercicio8App/src/main/java/com/globallogic/App.java
+++ modulo001/ejercicio8/Ejercicio8App/src/main/java/com/globallogic/App.java
@@ -10,11 +10,10 @@ public class App
     public static void main( String[] args )
     {
     	try {
-			throw new Exception("Esto es una Excepcion");
+			Object object = null;
+			object.toString();
 		} catch (Exception e) {
 			System.out.println("Se produjo ua excepcion: " + e.getMessage());
-		}finally {
-			System.out.println("Esto se ejecuta sin importar si se presentan errores");
 		}
     	
     }
