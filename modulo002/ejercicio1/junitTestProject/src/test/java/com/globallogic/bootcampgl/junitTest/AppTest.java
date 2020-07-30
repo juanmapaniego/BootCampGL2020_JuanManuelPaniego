e64bb19d2ad587886d20a278105d05aa10e74b92
Juan Manuel Paniego <juanma.paniego@gmail.com>
2020-07-29 21:20:29 -0300

    Resolved exercise 6 (Junit5)

--- modulo002/ejercicio1/junitTestProject/src/test/java/com/globallogic/bootcampgl/junitTest/AppTest.java
+++ modulo002/ejercicio1/junitTestProject/src/test/java/com/globallogic/bootcampgl/junitTest/AppTest.java
@@ -2,6 +2,7 @@ package com.globallogic.bootcampgl.junitTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
@@ -13,23 +14,38 @@ class AppTest {
 	
 	@Test
 	void ejer2() {
-		assertNotEquals("First", "First");
-		assertNotEquals("Second", "Second");
-		assertEquals("Third", "third");
+		assertEquals("First", "First");
+		assertEquals("Second", "Second");
+		assertNotEquals("Third", "third");
 	}
 
 	@Test
 	void ejer3() {
-		assertFalse(25 > 12);
-		assertTrue(25 > 27);
+		assertTrue(25 > 12);
+		assertFalse(25 > 27);
 	}
 	
 	@Test
 	void ejer4() {
 		Long index = null;
-		assertNotNull(index);
-		index = 1L;
 		assertNull(index);
+		index = 1L;
+		assertNotNull(index);
 		
 	}
+	
+	@Test
+	@DisplayName("El resultado esta bien")
+	void sumCorrectTest() {
+		App app = new App();
+		assertEquals(app.sum(3, 3), 3+3);
+	}
+	
+	@Test
+	@DisplayName("El resultado esta mal: ")
+	void sumIncorectTest() {
+		App app = new App();
+		assertNotEquals(app.sum(3, 3), 3+4);
+	}
+	
 }
