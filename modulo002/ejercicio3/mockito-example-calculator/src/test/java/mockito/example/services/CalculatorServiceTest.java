00013be36ec4529ad14136baf2f8125699719c43
Juan Manuel Paniego <juanma.paniego@gmail.com>
2020-07-31 17:48:42 -0300

    Resolved exercise 4 (Mockito)

--- modulo002/ejercicio3/mockito-example-calculator/src/test/java/mockito/example/services/CalculatorServiceTest.java
+++ modulo002/ejercicio3/mockito-example-calculator/src/test/java/mockito/example/services/CalculatorServiceTest.java
@@ -3,9 +3,13 @@
  */
 package mockito.example.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -18,13 +22,18 @@ import mockito.example.services.impl.CalculatorServiceImpl;
 public class CalculatorServiceTest {
 	@Mock
 	BasicOperationsService basicOperationsServiceMock;
+	@Mock
+	DataService dataServiceMock;
 	private CalculatorServiceImpl calculatorService;
+	
 	
 	@BeforeEach
 	void setup() {
 		calculatorService = new CalculatorServiceImpl();
 		calculatorService.setBasicOperationsService(basicOperationsServiceMock);
+		calculatorService.setDataService(dataServiceMock);
 	}
+	
 	@Test
 	void testAdd() {
 		Mockito.when(basicOperationsServiceMock.add(1.0,1.0)).thenReturn(2.0);
@@ -55,6 +64,14 @@ public class CalculatorServiceTest {
 		
 		calculatorService.calculateDivision(1, 1);
 		verify(basicOperationsServiceMock).divide(1.0, 1.0);
+	}
+	
+	@Test
+	@DisplayName("DataServiceMock")
+	void testDisplayList(){
+		Mockito.when(dataServiceMock.getListOfNumbers()).thenReturn(new int[] {1,2,3,4,5});
+		
+		assertEquals(3.0, calculatorService.calculateAverage());
 	}
 	
 	
