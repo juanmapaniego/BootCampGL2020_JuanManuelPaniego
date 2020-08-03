bb40e59b1c960455c4187eeb5bac2c20306082da
Juan Manuel Paniego <juanma.paniego@gmail.com>
2020-08-03 18:19:19 -0300

    Resolved exercise 2 (Singleton)

---
+++ modulo003/patterns/src/test/java/com/globallogic/bootcamp/patterns/singleton/CurrencyTest.java
@@ -0,0 +1,19 @@
+package com.globallogic.bootcamp.patterns.singleton;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CurrencyTest {
+
+	@Test
+	@DisplayName("Same Instance")
+	void test() {
+		Currency currency1 = Currency.getInstance();
+		Currency currency2 = Currency.getInstance();
+		
+		assertEquals(currency1, currency2);
+	}
+
+}
